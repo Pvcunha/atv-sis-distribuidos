404011d36e5af5c3bbda5bb5ad8edefe7fe4237d
gso1 <gso@cin.ufpe.br>
2024-07-21 22:43:57 -0300

    exec

--- sis-dis-ex1/grpc/client/exec.sh
+++ sis-dis-ex1/grpc/client/exec.sh
@@ -3,8 +3,11 @@
 go run ../server/server.go &
 sleep 5
 echo "step 1"
-go run client.go -run=100 > grpc_100.txt
+go run client.go -run=100 -conc=true > grpc_conc_100.txt
+go run client.go -run=100 -conc=false > grpc_100.txt
 echo "step 2"
-go run client.go -run=1000 > grpc_1000.txt
+go run client.go -run=1000 -conc=true > grpc_conc_1000.txt
+go run client.go -run=1000 -conc=false > grpc_1000.txt
 echo "step 3"
-go run client.go -run=10000 > grpc_10000.txt
+go run client.go -run=10000 -conc=true > grpc_conc_10000.txt
+go run client.go -run=10000 -conc=false > grpc_10000.txt
